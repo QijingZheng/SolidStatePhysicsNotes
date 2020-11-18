4a0557534bffdea8119585b9ff058fe14e2ae4fa
QijingZheng <zqj.kaka@gmail.com>
2020-11-18 09:47:21 +0800

    First commit: add files

---
+++ phonons/etc/auto/macros.el
@@ -0,0 +1,32 @@
+(TeX-add-style-hook
+ "macros"
+ (lambda ()
+   (TeX-add-symbols
+    '("zjball" ["argument"] 1)
+    '("colvec" ["argument"] 2)
+    '("info" ["argument"] 3)
+    '("tikzremember" 2)
+    '("tikzmark" 1)
+    '("rom" 1)
+    '("BOsup" 0)
+    '("onehalf" 0)
+    '("dt" 1)
+    '("Rvec" 0)
+    '("svec" 0)
+    '("rvec" 0)
+    '("hamil" 0)
+    '("schro" 0)
+    '("abinitio" 0)
+    "angstrom"
+    "rt"
+    "rR"
+    "prR"
+    "Rt"
+    "rRt"
+    "EYSR"
+    "DPSR"
+    "tr"
+    "shortminus"
+    "mathcolor"))
+ :latex)
+
---
+++ phonons/etc/auto/packages.el
@@ -0,0 +1,7 @@
+(TeX-add-style-hook
+ "packages"
+ (lambda ()
+   (TeX-add-symbols
+    "mathcolor"))
+ :plain-tex)
+
